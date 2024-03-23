892ae2a677def65f54c093138c46933bd130b9f3
LarsMans <l.s.vanemmerik@gmail.com>
2024-03-23 16:17:50 +0100

    return to working panels

--- src/main/java/net/portalmod/common/sorted/panel/PanelBlock.java
+++ src/main/java/net/portalmod/common/sorted/panel/PanelBlock.java
@@ -47,27 +47,27 @@ public class PanelBlock extends Block {
 
         // Block is single and placing on top or bottom
         if (clickedPanelState.isSingle() && !isSide(clickedFace)) {
-//            world.setBlockAndUpdate(clickedOnPos, clickedBlock.setValue(STATE, clickedFace == Direction.UP ? PanelState.BOTTOM : PanelState.TOP));
+            world.setBlockAndUpdate(clickedOnPos, clickedBlock.setValue(STATE, clickedFace == Direction.UP ? PanelState.BOTTOM : PanelState.TOP));
             return this.defaultBlockState().setValue(STATE, clickedFace == Direction.UP ? PanelState.TOP : PanelState.BOTTOM);
         }
 
         // Block is double and placing on side
         if (this.canBeBig && clickedPanelState.isDouble() && isSide(clickedFace) && isBlockInInventoryAndRemove(context.getPlayer(), this)) {
 //
-//            // New block
-//            world.setBlock(context.getClickedPos().relative(clickedPanelState.getVerticalFacing()), clickedBlock
-//                    .setValue(STATE, directionalState(clickedFace, clickedPanelState.opposite()))
-//                    .setValue(AXIS, clickedFace.getAxis()), 0);
-//
-//            // Block under/above clicked block
-//            world.setBlock(clickedOnPos.relative(clickedPanelState.getVerticalFacing()), clickedBlock
-//                    .setValue(STATE, directionalState(clickedFace.getOpposite(), clickedPanelState.opposite()))
-//                    .setValue(AXIS, clickedFace.getAxis()), 0);
-//
-//            // Clicked block
-//            world.setBlock(clickedOnPos, clickedBlock
-//                    .setValue(STATE, directionalState(clickedFace.getOpposite(), clickedPanelState))
-//                    .setValue(AXIS, clickedFace.getAxis()), 0);
+            // New block
+            world.setBlock(context.getClickedPos().relative(clickedPanelState.getVerticalFacing()), clickedBlock
+                    .setValue(STATE, directionalState(clickedFace, clickedPanelState.oppositeVertical()))
+                    .setValue(AXIS, clickedFace.getAxis()), 0);
+
+            // Block under/above clicked block
+            world.setBlock(clickedOnPos.relative(clickedPanelState.getVerticalFacing()), clickedBlock
+                    .setValue(STATE, directionalState(clickedFace.getOpposite(), clickedPanelState.oppositeVertical()))
+                    .setValue(AXIS, clickedFace.getAxis()), 0);
+
+            // Clicked block
+            world.setBlock(clickedOnPos, clickedBlock
+                    .setValue(STATE, directionalState(clickedFace.getOpposite(), clickedPanelState))
+                    .setValue(AXIS, clickedFace.getAxis()), 0);
 
             return this.defaultBlockState()
                     .setValue(STATE, directionalState(clickedFace, clickedPanelState))
@@ -79,29 +79,29 @@ public class PanelBlock extends Block {
 
     @Override
     public void onPlace(BlockState blockState, World world, BlockPos pos, BlockState p_220082_4_, boolean p_220082_5_) {
-        PanelState panelState = blockState.getValue(STATE);
-
-        // Place the other half
-        if (panelState.isDouble()) {
-            world.setBlockAndUpdate(pos.relative(panelState.getVerticalFacing()), blockState.setValue(STATE, panelState.oppositeVertical()));
-            return;
-        }
-
-        Direction.Axis axis = blockState.getValue(AXIS);
-
-        // Place the other 3 blocks
-        if (panelState.isQuadruple()) {
-            Direction direction = Direction.fromAxisAndDirection(axis, panelState.isLeft() ? Direction.AxisDirection.POSITIVE : Direction.AxisDirection.NEGATIVE);
-
-            // Clicked block
-            setBlockUnlessStateMatches(world, pos, directionalState(direction.getOpposite(), panelState), axis);
-
-            // New block
-            setBlockUnlessStateMatches(world, pos.relative(panelState.getVerticalFacing()), directionalState(direction, panelState.oppositeVertical()), axis);
-
-            // Block under/above clicked block
-            setBlockUnlessStateMatches(world, pos.relative(panelState.getVerticalFacing()), directionalState(direction.getOpposite(), panelState.oppositeVertical()), axis);
-        }
+//        PanelState panelState = blockState.getValue(STATE);
+//
+//        // Place the other half
+//        if (panelState.isDouble()) {
+//            world.setBlockAndUpdate(pos.relative(panelState.getVerticalFacing()), blockState.setValue(STATE, panelState.oppositeVertical()));
+//            return;
+//        }
+//
+//        Direction.Axis axis = blockState.getValue(AXIS);
+//
+//        // Place the other 3 blocks
+//        if (panelState.isQuadruple()) {
+//            Direction direction = Direction.fromAxisAndDirection(axis, panelState.isLeft() ? Direction.AxisDirection.POSITIVE : Direction.AxisDirection.NEGATIVE);
+//
+//            // Clicked block
+//            setBlockUnlessStateMatches(world, pos, directionalState(direction.getOpposite(), panelState), axis);
+//
+//            // New block
+//            setBlockUnlessStateMatches(world, pos.relative(panelState.getVerticalFacing()), directionalState(direction, panelState.oppositeVertical()), axis);
+//
+//            // Block under/above clicked block
+//            setBlockUnlessStateMatches(world, pos.relative(panelState.getVerticalFacing()), directionalState(direction.getOpposite(), panelState.oppositeVertical()), axis);
+//        }
     }
 
     public static boolean isSide(Direction direction) {
