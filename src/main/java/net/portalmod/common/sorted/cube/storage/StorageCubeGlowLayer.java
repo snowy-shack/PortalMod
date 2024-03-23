1e844016d48038edbec9b7a081d88ee429f1ff59
LarsMans <l.s.vanemmerik@gmail.com>
2024-03-23 17:45:48 +0100

    glow layer light level fix

--- src/main/java/net/portalmod/common/sorted/cube/storage/StorageCubeGlowLayer.java
+++ src/main/java/net/portalmod/common/sorted/cube/storage/StorageCubeGlowLayer.java
@@ -3,9 +3,9 @@ package net.portalmod.common.sorted.cube.storage;
 import com.mojang.blaze3d.matrix.MatrixStack;
 import com.mojang.blaze3d.vertex.IVertexBuilder;
 import net.minecraft.client.renderer.IRenderTypeBuffer;
+import net.minecraft.client.renderer.LightTexture;
 import net.minecraft.client.renderer.RenderType;
 import net.minecraft.client.renderer.entity.IEntityRenderer;
-import net.minecraft.client.renderer.entity.layers.AbstractEyesLayer;
 import net.minecraft.client.renderer.entity.layers.LayerRenderer;
 import net.minecraft.client.renderer.texture.OverlayTexture;
 import net.minecraft.util.ResourceLocation;
@@ -23,6 +23,9 @@ public class StorageCubeGlowLayer<T extends Cube> extends LayerRenderer<T, CubeM
 
     public void render(MatrixStack matrixStack, IRenderTypeBuffer renderTypeBuffer, int i, T cube, float f1, float f2, float f3, float f4, float f5, float f6) {
         IVertexBuilder ivertexbuilder = renderTypeBuffer.getBuffer(cube.isActive() ? STORAGE_CUBE_ACTIVE_GLOW : STORAGE_CUBE_GLOW);
-        this.getParentModel().renderToBuffer(matrixStack, ivertexbuilder, 200, OverlayTexture.NO_OVERLAY, 1.0F, 1.0F, 1.0F, 1.0F);
+        this.getParentModel().renderToBuffer(matrixStack, ivertexbuilder, LightTexture.pack(
+                Math.max(LightTexture.block(i), 9),
+                Math.max(LightTexture.sky(i), 9)
+        ), OverlayTexture.NO_OVERLAY, 1.0F, 1.0F, 1.0F, 1.0F);
     }
 }
