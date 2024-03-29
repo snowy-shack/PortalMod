7696ebf2604005924858b710429690a2d3cef6d3
LarsMans <l.s.vanemmerik@gmail.com>
2024-03-29 15:03:04 +0100

    Chamber Lights drops

--- src/main/java/net/portalmod/common/blocks/ChamberLightsBlock.java
+++ src/main/java/net/portalmod/common/blocks/ChamberLightsBlock.java
@@ -4,12 +4,14 @@ import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.Blocks;
 import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.BlockItemUseContext;
 import net.minecraft.item.ItemStack;
 import net.minecraft.state.EnumProperty;
 import net.minecraft.state.StateContainer.Builder;
 import net.minecraft.state.properties.BlockStateProperties;
 import net.minecraft.state.properties.DoubleBlockHalf;
+import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.Direction;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.IWorld;
@@ -62,5 +64,31 @@ public class ChamberLightsBlock extends Block {
         return blockState;
     }
 
+    public void playerWillDestroy(World world, BlockPos pos, BlockState blockState, PlayerEntity player) {
+        if (!world.isClientSide) {
+            if (player.isCreative()) {
+                preventCreativeDropFromBottomPart(world, pos, blockState, player);
+            } else {
+                dropResources(blockState, world, pos, null, player, player.getMainHandItem());
+            }
+        }
 
+        super.playerWillDestroy(world, pos, blockState, player);
+    }
+
+    public void playerDestroy(World world, PlayerEntity player, BlockPos pos, BlockState blockState, @Nullable TileEntity tileEntity, ItemStack itemStack) {
+        super.playerDestroy(world, player, pos, Blocks.AIR.defaultBlockState(), tileEntity, itemStack);
+    }
+
+    protected static void preventCreativeDropFromBottomPart(World world, BlockPos pos, BlockState blockState, PlayerEntity player) {
+        DoubleBlockHalf doubleblockhalf = blockState.getValue(HALF);
+        if (doubleblockhalf == DoubleBlockHalf.UPPER) {
+            BlockPos blockpos = pos.below();
+            BlockState blockstate = world.getBlockState(blockpos);
+            if (blockstate.getBlock() == blockState.getBlock() && blockstate.getValue(HALF) == DoubleBlockHalf.LOWER) {
+                world.setBlock(blockpos, Blocks.AIR.defaultBlockState(), 35);
+                world.levelEvent(player, 2001, blockpos, Block.getId(blockstate));
+            }
+        }
+    }
 }
