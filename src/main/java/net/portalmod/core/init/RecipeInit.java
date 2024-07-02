f8e7d70a9c94d858a0fbc190b4b9e3719781b15c
PhantomEye <armanddevries@outlook.com>
2024-07-02 15:35:26 +0200

    Portal Gun recipes

--- src/main/java/net/portalmod/core/init/RecipeInit.java
+++ src/main/java/net/portalmod/core/init/RecipeInit.java
@@ -11,5 +11,5 @@ import net.portalmod.common.sorted.portalgun.PortalGunRecipe;
 public class RecipeInit {
     public static final DeferredRegister<IRecipeSerializer<?>> RECIPES = DeferredRegister.create(ForgeRegistries.RECIPE_SERIALIZERS, PortalMod.MODID);
 
-    public static final RegistryObject<SpecialRecipeSerializer<PortalGunRecipe>> PORTAL_GUN = RECIPES.register("portal_gun_modifying", () -> new SpecialRecipeSerializer<>(PortalGunRecipe::new));
+    public static final RegistryObject<SpecialRecipeSerializer<PortalGunRecipe>> PORTAL_GUN = RECIPES.register("portalgun_modifying", () -> new SpecialRecipeSerializer<>(PortalGunRecipe::new));
 }
