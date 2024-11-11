5a37f058dffbfb47a4d60ac49cd053259a130fa1
Phantom <armand.devries@outlook.com>
2024-11-11 14:36:54 +0100

    Throw Turrets

--- src/main/java/net/portalmod/common/sorted/portalgun/PortalGunClient.java
+++ src/main/java/net/portalmod/common/sorted/portalgun/PortalGunClient.java
@@ -1,13 +1,13 @@
 package net.portalmod.common.sorted.portalgun;
 
 import net.minecraft.client.Minecraft;
-import net.portalmod.common.sorted.cube.Cube;
+import net.portalmod.common.entities.TestElementEntity;
 import net.portalmod.common.sorted.portal.PortalEnd;
 import net.portalmod.core.init.PacketInit;
 
 public class PortalGunClient {
     protected static void handleLeftClick() {
-        if(Minecraft.getInstance().player.hasPassenger(Cube.class)) {
+        if(Minecraft.getInstance().player.hasPassenger(TestElementEntity.class)) {
             PortalGun.dropCube(Minecraft.getInstance().player, true);
             PacketInit.INSTANCE.sendToServer(new CPortalGunInteractionPacket.Builder(PortalGunInteraction.THROW_ENTITY).build());
         } else PacketInit.INSTANCE.sendToServer(new CPortalGunInteractionPacket.Builder(PortalGunInteraction.SHOOT_PORTAL).end(PortalEnd.PRIMARY).build());
