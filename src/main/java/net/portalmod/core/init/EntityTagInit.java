c0d28c0ce195e3a44ddb9a48695c0a93b5ba5c57
Phantom <armand.devries@outlook.com>
2024-11-15 00:29:55 +0100

    Button Lightweight tag

--- src/main/java/net/portalmod/core/init/EntityTagInit.java
+++ src/main/java/net/portalmod/core/init/EntityTagInit.java
@@ -11,6 +11,7 @@ public class EntityTagInit {
     public static void init() {}
     
     public static final Tags.IOptionalNamedTag<EntityType<?>> FIZZLER_NO_ITEM_DROPS = blockTag("fizzler_no_item_drops");
+    public static final Tags.IOptionalNamedTag<EntityType<?>> BUTTON_NO_PRESS = blockTag("super_button_lightweight");
 
     public static Tags.IOptionalNamedTag<EntityType<?>> blockTag(String name) {
         return EntityTypeTags.createOptional(new ResourceLocation(PortalMod.MODID, name));
