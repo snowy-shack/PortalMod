8fa862ea87ccc54382ea2eb7cd2c84c8b5bd791d
LarsMans <l.s.vanemmerik@gmail.com>
2024-11-15 18:03:24 +0100

    Small flame particle backport

---
+++ src/main/java/net/portalmod/mixins/accessors/FlameParticleAccessor.java
@@ -0,0 +1,14 @@
+package net.portalmod.mixins.accessors;
+
+import net.minecraft.client.particle.FlameParticle;
+import net.minecraft.client.world.ClientWorld;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.gen.Invoker;
+
+@Mixin(FlameParticle.class)
+public interface FlameParticleAccessor {
+    @Invoker(remap = false, value = "<init>")
+    static FlameParticle pmInit(ClientWorld world, double a, double b, double c, double d, double e, double f) {
+        throw new AssertionError();
+    }
+}
