5d89cb95b6061df6311d6f4e4c8d5b3fd0502a46
LarsMans <l.s.vanemmerik@gmail.com>
2024-12-30 18:01:32 +0100

    Delete conversion for once and for all

--- src/main/java/net/portalmod/common/sorted/gel/ConversionGelBlock.java
+++
@@ -1,7 +0,0 @@
-package net.portalmod.common.sorted.gel;
-
-public class ConversionGelBlock extends AbstractGelBlock {
-    public ConversionGelBlock(Properties properties) {
-        super(properties);
-    }
-}
