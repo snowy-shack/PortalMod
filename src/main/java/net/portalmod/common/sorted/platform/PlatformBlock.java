5992522d0bae0f86ced32d709d60df438503855d
Phantom <armand.devries@outlook.com>
2025-03-28 16:11:33 +0100

    Platform block powering

--- src/main/java/net/portalmod/common/sorted/platform/PlatformBlock.java
+++ src/main/java/net/portalmod/common/sorted/platform/PlatformBlock.java
@@ -2,6 +2,7 @@ package net.portalmod.common.sorted.platform;
 
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
+import net.minecraft.block.BreakableBlock;
 import net.minecraft.block.IWaterLoggable;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.fluid.FluidState;
@@ -22,18 +23,23 @@ import net.minecraft.util.math.BlockRayTraceResult;
 import net.minecraft.util.math.shapes.ISelectionContext;
 import net.minecraft.util.math.shapes.VoxelShape;
 import net.minecraft.util.math.shapes.VoxelShapes;
+import net.minecraft.util.math.vector.Vector3f;
 import net.minecraft.util.text.TranslationTextComponent;
 import net.minecraft.world.IBlockReader;
 import net.minecraft.world.IWorld;
 import net.minecraft.world.World;
 import net.portalmod.common.items.WrenchItem;
+import net.portalmod.core.math.Mat4;
+import net.portalmod.core.math.Vec3;
+import net.portalmod.core.math.VoxelShapeGroup;
 
 import javax.annotation.Nullable;
 
-public class PlatformBlock extends Block implements IWaterLoggable {
+public class PlatformBlock extends BreakableBlock implements IWaterLoggable {
 
     public static final DirectionProperty FACING = BlockStateProperties.FACING;
     public static final EnumProperty<Half> HALF = BlockStateProperties.HALF;
+    public static final EnumProperty<Half> ORIGINAL_HALF = EnumProperty.create("original_half", Half.class);
     public static final BooleanProperty PILLAR = BooleanProperty.create("pillar");
     public static final BooleanProperty FORCED_PILLAR = BooleanProperty.create("forced_pillar");
     public static final BooleanProperty WATERLOGGED = BlockStateProperties.WATERLOGGED;
@@ -43,14 +49,16 @@ public class PlatformBlock extends Block implements IWaterLoggable {
         this.registerDefaultState(this.stateDefinition.any()
                 .setValue(FACING, Direction.UP)
                 .setValue(HALF, Half.BOTTOM)
+                .setValue(ORIGINAL_HALF, Half.BOTTOM)
                 .setValue(PILLAR, false)
                 .setValue(FORCED_PILLAR, false)
-                .setValue(WATERLOGGED, false));
+                .setValue(WATERLOGGED, false)
+        );
     }
 
     @Override
     protected void createBlockStateDefinition(StateContainer.Builder<Block, BlockState> builder) {
-        builder.add(FACING, HALF, PILLAR, FORCED_PILLAR, WATERLOGGED);
+        builder.add(FACING, HALF, PILLAR, ORIGINAL_HALF, FORCED_PILLAR, WATERLOGGED);
     }
 
     @Override
@@ -58,8 +66,7 @@ public class PlatformBlock extends Block implements IWaterLoggable {
         if (player.getItemInHand(hand).getItem() instanceof PillarItem || WrenchItem.usedWrench(player, hand)) {
 
             if (hasPillarBelow(state, world, pos)) {
-                player.displayClientMessage(new TranslationTextComponent("actionbar.portalmod.step.fail"), true);
-                return ActionResultType.SUCCESS;
+                return ActionResultType.FAIL;
             }
 
             BlockState cycled = state.cycle(FORCED_PILLAR);
@@ -79,7 +86,30 @@ public class PlatformBlock extends Block implements IWaterLoggable {
 
     @Override
     public VoxelShape getShape(BlockState blockState, IBlockReader blockReader, BlockPos pos, ISelectionContext context) {
-        return VoxelShapes.block(); // TODO add actual shape
+        double raise = blockState.getValue(HALF) == Half.BOTTOM ? 0 : 8;
+        VoxelShapeGroup SHAPE_PLATFORM = new VoxelShapeGroup.Builder()
+                .add(0, 3 + raise, 0, 16, 8 + raise, 16)
+                .build();
+        VoxelShapeGroup SHAPE_PILLAR = new VoxelShapeGroup.Builder()
+                .add(5, 0, 5, 11, 3 + raise, 11)
+                .add(0, 3 + raise, 0, 16, 8 + raise, 16)
+                .build();
+
+        VoxelShapeGroup combined = blockState.getValue(PILLAR) ? SHAPE_PILLAR : SHAPE_PLATFORM;
+
+        // Rotate according to FACING
+        int angleX = blockState.getValue(FACING) == Direction.UP
+                ? 0 : blockState.getValue(FACING) == Direction.DOWN
+                    ? 180 : 90;
+        int angleY = blockState.getValue(FACING).get2DDataValue() * 90;
+
+        Mat4 matrix = Mat4.identity();
+        matrix.translate(new Vec3(.5));
+        matrix.rotateDeg(Vector3f.YN, angleY);
+        matrix.rotateDeg(Vector3f.XP, angleX);
+        matrix.translate(new Vec3(-.5));
+
+        return combined.transform(matrix).getShape();
     }
 
     @Nullable
@@ -91,15 +121,8 @@ public class PlatformBlock extends Block implements IWaterLoggable {
                 .setValue(WATERLOGGED, waterlogged)
                 .setValue(FACING, context.getClickedFace());
 
-        boolean bottom;
-        if (clickedFace.getAxis() == Direction.Axis.Y && context.getPlayer() != null) {
-            // Swap top or bottom when holding shift
-            bottom = (clickedFace.getAxisDirection() == Direction.AxisDirection.POSITIVE) != context.getPlayer().isShiftKeyDown();
-        } else {
-            bottom = context.getClickLocation().y - context.getClickedPos().getY() < 0.5;
-        }
-
-        return state.setValue(HALF, bottom ? Half.BOTTOM : Half.TOP)
+        return state.setValue(HALF, context.getPlayer().isShiftKeyDown() ? Half.BOTTOM : Half.TOP)
+                .setValue(ORIGINAL_HALF, context.getPlayer().isShiftKeyDown() ? Half.BOTTOM : Half.TOP)
                 .setValue(PILLAR, hasPillarBelow(state, context.getLevel(), context.getClickedPos()));
     }
 
@@ -109,7 +132,23 @@ public class PlatformBlock extends Block implements IWaterLoggable {
             world.getLiquidTicks().scheduleTick(pos, Fluids.WATER, Fluids.WATER.getTickDelay(world));
         }
 
-        return blockState.setValue(PILLAR, shouldHavePillar(blockState, world, pos));
+        BlockState newState = blockState.setValue(PILLAR, shouldHavePillar(blockState, world, pos));
+
+        return newState;
+    }
+
+    @Override
+    public void neighborChanged(BlockState state, World level, BlockPos pos, Block block, BlockPos neighborPos, boolean b) {
+        super.neighborChanged(state, level, pos, block, neighborPos, b);
+
+        boolean power = level.hasNeighborSignal(pos);
+
+        Half oldHalf = state.getValue(HALF);
+        Half newHalf = power ? Half.TOP : state.getValue(ORIGINAL_HALF);
+
+        if (newHalf != oldHalf) {
+            level.setBlockAndUpdate(pos, state.setValue(HALF, newHalf));
+        }
     }
 
     public static boolean shouldHavePillar(BlockState blockState, IWorld world, BlockPos pos) {
