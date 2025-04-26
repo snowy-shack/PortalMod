82a17605403d78f41fef4f761a9d51b8fc012dc9
LarsMans <l.s.vanemmerik@gmail.com>
2025-04-26 13:08:44 +0200

    Make tile entities easer to add for addons

--- src/main/java/net/portalmod/core/init/TileEntityTypeInit.java
+++ src/main/java/net/portalmod/core/init/TileEntityTypeInit.java
@@ -1,5 +1,6 @@
 package net.portalmod.core.init;
 
+import net.minecraft.block.Block;
 import net.minecraft.tileentity.TileEntityType;
 import net.minecraftforge.fml.RegistryObject;
 import net.minecraftforge.registries.DeferredRegister;
@@ -12,26 +13,76 @@ import net.portalmod.common.sorted.faithplate.FaithPlateTileEntity;
 import net.portalmod.common.sorted.fizzler.FizzlerEmitterTileEntity;
 import net.portalmod.common.sorted.radio.RadioBlockTileEntity;
 
+import java.util.HashSet;
+import java.util.Set;
+import java.util.function.UnaryOperator;
+
+@SuppressWarnings("DataFlowIssue")
 public class TileEntityTypeInit {
     public static final DeferredRegister<TileEntityType<?>> TILE_ENTITY_TYPES = DeferredRegister.create(ForgeRegistries.TILE_ENTITIES, PortalMod.MODID);
 
-    private TileEntityTypeInit() {}
-    
     public static final RegistryObject<TileEntityType<RadioBlockTileEntity>> RADIO = TILE_ENTITY_TYPES.register("radio",
-            () -> TileEntityType.Builder.of(RadioBlockTileEntity::new, BlockInit.RADIO.get()).build(null));
+            () -> TileEntityType.Builder.of(RadioBlockTileEntity::new,
+                    getBlocks(TileEntityTypeInit::getRadioBlocks)).build(null));
+
 
     public static final RegistryObject<TileEntityType<AntlineTileEntity>> ANTLINE = TILE_ENTITY_TYPES.register("antline",
-            () -> TileEntityType.Builder.of(AntlineTileEntity::new, BlockInit.ANTLINE.get()).build(null));
+            () -> TileEntityType.Builder.of(AntlineTileEntity::new,
+                    getBlocks(TileEntityTypeInit::getAntlineBlocks)).build(null));
+
 
     public static final RegistryObject<TileEntityType<FaithPlateTileEntity>> FAITHPLATE = TILE_ENTITY_TYPES.register("faithplate",
-            () -> TileEntityType.Builder.of(FaithPlateTileEntity::new, BlockInit.FAITHPLATE.get()).build(null));
+            () -> TileEntityType.Builder.of(FaithPlateTileEntity::new,
+                    getBlocks(TileEntityTypeInit::getFaithplateBlocks)).build(null));
+
 
     public static final RegistryObject<TileEntityType<ChamberDoorTileEntity>> CHAMBER_DOOR = TILE_ENTITY_TYPES.register("chamber_door",
-            () -> TileEntityType.Builder.of(ChamberDoorTileEntity::new, BlockInit.CHAMBER_DOOR.get()).build(null));
+            () -> TileEntityType.Builder.of(ChamberDoorTileEntity::new,
+                    getBlocks(TileEntityTypeInit::getChamberDoorBlocks)).build(null));
+
 
     public static final RegistryObject<TileEntityType<CubeDropperTileEntity>> CUBE_DROPPER = TILE_ENTITY_TYPES.register("cube_dropper",
-            () -> TileEntityType.Builder.of(CubeDropperTileEntity::new, BlockInit.CUBE_DROPPER.get()).build(null));
+            () -> TileEntityType.Builder.of(CubeDropperTileEntity::new,
+                    getBlocks(TileEntityTypeInit::getCubeDropperBlocks)).build(null));
+
 
     public static final RegistryObject<TileEntityType<FizzlerEmitterTileEntity>> FIZZLER_EMITTER = TILE_ENTITY_TYPES.register("fizzler_emitter",
-            () -> TileEntityType.Builder.of(FizzlerEmitterTileEntity::new, BlockInit.FIZZLER_EMITTER.get()).build(null));
+            () -> TileEntityType.Builder.of(FizzlerEmitterTileEntity::new,
+                    getBlocks(TileEntityTypeInit::getFizzlerEmitterBlocks)).build(null));
+
+
+
+    public static Block[] getBlocks(UnaryOperator<Set<Block>> function) {
+        return function.apply(new HashSet<>()).toArray(new Block[]{});
+    }
+
+    public static Set<Block> getRadioBlocks(Set<Block> blocks) {
+        blocks.add(BlockInit.RADIO.get());
+        return blocks;
+    }
+
+    public static Set<Block> getAntlineBlocks(Set<Block> blocks) {
+        blocks.add(BlockInit.ANTLINE.get());
+        return blocks;
+    }
+
+    public static Set<Block> getFaithplateBlocks(Set<Block> blocks) {
+        blocks.add(BlockInit.FAITHPLATE.get());
+        return blocks;
+    }
+
+    public static Set<Block> getChamberDoorBlocks(Set<Block> blocks) {
+        blocks.add(BlockInit.CHAMBER_DOOR.get());
+        return blocks;
+    }
+
+    public static Set<Block> getCubeDropperBlocks(Set<Block> blocks) {
+        blocks.add(BlockInit.CUBE_DROPPER.get());
+        return blocks;
+    }
+
+    public static Set<Block> getFizzlerEmitterBlocks(Set<Block> blocks) {
+        blocks.add(BlockInit.FIZZLER_EMITTER.get());
+        return blocks;
+    }
 }
