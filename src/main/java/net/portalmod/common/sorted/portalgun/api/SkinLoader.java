86adb027c1bb6c63027309f67b4b6f2dc60cbd8b
serialsniper <mome.broili@gmail.com>
2025-05-15 01:20:24 +0200

    Temporary Skin Loader

---
+++ src/main/java/net/portalmod/common/sorted/portalgun/api/SkinLoader.java
@@ -0,0 +1,28 @@
+package net.portalmod.common.sorted.portalgun.api;
+
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.renderer.texture.AtlasTexture;
+import net.minecraft.client.renderer.texture.TextureManager;
+import net.minecraft.util.ResourceLocation;
+import net.portalmod.PortalMod;
+import net.portalmod.client.animation.AnimatedTexture;
+
+public class SkinLoader {
+    public static AnimatedTexture loadSkin(String skinId) {
+        AnimatedTexture animatedTexture;
+
+        try {
+            SkinTexture skinTexture = new SkinTexture(skinId);
+            TextureManager tm = Minecraft.getInstance().textureManager;
+            tm.register(new ResourceLocation(PortalMod.MODID, "gun/test"), skinTexture);
+
+            animatedTexture = new AnimatedTexture(AtlasTexture.LOCATION_BLOCKS,
+                    new ResourceLocation(PortalMod.MODID, "gun/test"), skinTexture.getFrameCount(), 2);
+
+            return animatedTexture;
+        } catch(Exception e) {
+        }
+
+        return null;
+    }
+}
