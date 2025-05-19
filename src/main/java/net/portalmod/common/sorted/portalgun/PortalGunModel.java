61db24dd26d5e7f5bc5816a226058485ac3ec411
Phantom <armand.devries@outlook.com>
2025-05-19 21:36:59 +0200

    Better Portal Gun UV + PotatOS!!

--- src/main/java/net/portalmod/common/sorted/portalgun/PortalGunModel.java
+++ src/main/java/net/portalmod/common/sorted/portalgun/PortalGunModel.java
@@ -14,45 +14,69 @@ public class PortalGunModel extends AnimatedModel {
     // todo animations for left hand
 
     public PortalGunModel() {
-        super(32, 16);
+        super(32, 32); // New texture
 
         gun = new ModelRenderer(this);
         {
-            ModelRenderer gun_front = new ModelRenderer(this);
+            ModelRenderer gun_front = new ModelRenderer(this); // TODO DONE
             gun_front.setPos(0.0F, 24.0F, 0.0F);
-            gun_front.texOffs(16, 5).addBox(-2.0F, -3.5F, -3.0F, 4.0F, 3.0F, 4.0F, 0.0F, false);
-            gun_front.texOffs(0, 2).addBox(-1.0F, -4.0F, -4.0F, 2.0F, 2.0F, 2.0F, 0.0F, false);
-            gun_front.texOffs(23, 12).addBox(-1.5F, -4.5F, -2.0F, 3.0F, 3.0F, 1.0F, 0.0F, false);
-            gun_front.texOffs(16, 8).addBox(-1.0F, -2.0F, -3.0F, 2.0F, 1.0F, 0.0F, 0.0F, false);
+            gun_front.texOffs(16, 13).addBox(-2.0F, -3.5F, -3.0F, 4.0F, 3.0F, 4.0F, 0.0F, false);
+            gun_front.texOffs(18, 22).addBox(-1.0F, -4.0F, -4.0F, 2.0F, 2.0F, 2.0F, 0.0F, false);
+            gun_front.texOffs(11, 12).addBox(-1.5F, -4.5F, -2.0F, 3.0F, 3.0F, 1.0F, 0.0F, false);
+            gun_front.texOffs(21, 20).addBox(-1.0F, -2.0F, -3.0F, 2.0F, 1.0F, 0.0F, 0.0F, false);
 
-            ModelRenderer prong_right = new ModelRenderer(this);
+            ModelRenderer prong_right = new ModelRenderer(this); // TODO DONE ??
             prong_right.setPos(-1.2985F, -2.2491F, -2.5F);
             gun_front.addChild(prong_right);
             setRotationAngle(prong_right, -1.5708F, 0.0F, -2.0944F);
-            prong_right.texOffs(0, 4).addBox(0.067F, -0.5F, -2.616F, 0.0F, 5.0F, 3.0F, 0.0F, true);
+            prong_right.texOffs(11, 3).addBox(0.067F, -0.5F, -2.616F, 0.0F, 5.0F, 3.0F, 0.0F, true);
 
-            ModelRenderer prong_left = new ModelRenderer(this);
+            ModelRenderer prong_left = new ModelRenderer(this); // TODO DONE ??
             prong_left.setPos(1.2985F, -2.2491F, -2.5F);
             gun_front.addChild(prong_left);
             setRotationAngle(prong_left, -1.5708F, 0.0F, 2.0944F);
-            prong_left.texOffs(0, 4).addBox(-0.067F, -0.5F, -2.616F, 0.0F, 5.0F, 3.0F, 0.0F, false);
+            prong_left.texOffs(18, 3).addBox(-0.067F, -0.5F, -2.616F, 0.0F, 5.0F, 3.0F, 0.0F, false);
 
-            ModelRenderer prong_top = new ModelRenderer(this);
+            ModelRenderer prong_top = new ModelRenderer(this); // TODO DONE ??
             prong_top.setPos(0.0F, -4.0F, -2.0F);
             gun_front.addChild(prong_top);
             setRotationAngle(prong_top, -1.5708F, 0.0F, 0.0F);
-            prong_top.texOffs(0, 4).addBox(0.0F, -0.5F, -3.0F, 0.0F, 5.0F, 3.0F, 0.0F, false);
+            prong_top.texOffs(25, 3).addBox(0.0F, -0.5F, -3.0F, 0.0F, 5.0F, 3.0F, 0.0F, false);
 
-            ModelRenderer gun_base = new ModelRenderer(this);
+
+            // position of whole patat
+            ModelRenderer potatoParent = new ModelRenderer(this);
+            potatoParent.setPos(0f, 2f, -3f);
+            prong_top.addChild(potatoParent);
+            setRotationAngle(potatoParent, 1.7F, 0.0F, 0.4F);
+
+            // the potato itself
+            ModelRenderer potato = new ModelRenderer(this);
+            potato.setPos(-0.2206F, -0.0222F, -0.3924F);
+            potatoParent.addChild(potato);
+            setRotationAngle(potato, -0.8248F, -0.6669F, 0.9374F);
+            potato.texOffs(21, 1).addBox(-1.3119F, -1.0513F, -1.1565F, 3.0F, 2.0F, 2.0F, 0.0F, false);
+
+            // the transparent plane
+            ModelRenderer potatoWires = new ModelRenderer(this);
+            potatoWires.setPos(-0.2018F, -0.067F, -0.7244F);
+            potatoParent.addChild(potatoWires);
+            setRotationAngle(potatoWires, 2.5876F, -0.0168F, -2.7786F);
+            potatoWires.texOffs(12, 1).addBox(-2.3359F, -1.9776F, -0.3457F, 4.0F, 4.0F, 0.0F, 0.0F, false);
+
+//            prong_top.addChild(potatoParent);
+
+
+            ModelRenderer gun_base = new ModelRenderer(this); // ?? - TODO DONE
             gun_base.setPos(0.0F, 24.0F, 0.0F);
-            gun_base.texOffs(0, 6).addBox(-2.5F, -5.5F, 2.0F, 5.0F, 4.0F, 6.0F, 0.0F, false);
-            gun_base.texOffs(8, 1).addBox(-1.5F, -3.0F, 4.0F, 3.0F, 2.0F, 3.0F, 0.0F, false);
+            gun_base.texOffs(1, 21).addBox(-2.5F, -5.5F, 2.0F, 5.0F, 4.0F, 6.0F, 0.0F, false); // Big white back
+            gun_base.texOffs(1, 14).addBox(-1.5F, -3.0F, 4.0F, 3.0F, 2.0F, 3.0F, 0.0F, false);
 
             ModelRenderer bottom = new ModelRenderer(this);
             bottom.setPos(0.0F, -2.0F, 2.5F);
             gun_base.addChild(bottom);
-            setRotationAngle(bottom, 0.0F, 3.1416F, 0.0F);
-            bottom.texOffs(8, 1).addBox(-1.5F, -1.0F, -1.5F, 3.0F, 2.0F, 3.0F, 0.0F, false);
+            setRotationAngle(bottom, 0.0F, 3.1416F, 0.0F); // TODO duplicates???
+            bottom.texOffs(1, 14).addBox(-1.5F, -1.0F, -1.5F, 3.0F, 2.0F, 3.0F, 0.0F, false);
 
             gun.addChild(gun_front);
             gun.addChild(gun_base);
@@ -76,13 +100,13 @@ public class PortalGunModel extends AnimatedModel {
         colour = new ModelRenderer(this);
         {
             ModelRenderer colour_front = new ModelRenderer(this);
-            colour_front.setPos(0.0F, 24.0F, 0.0F);
-            colour_front.texOffs(0, 0).addBox(-1.0F, -4.0F, -1.0F, 2.0F, 2.0F, 3.0F, 0.0F, false); //color_tube
-            colour_front.texOffs(8, 1).addBox(-0.5F, -3.5F, -2.0F, 1.0F, 1.0F, 0.0F, 0.0F, false); //color_front
+            colour_front.setPos(0.0F, 24.0F, 0.0F); // 1: The glass tube, 2: Operational end of the device
+            colour_front.texOffs(1, 6).addBox(-1.0F, -4.0F, -1.0F, 2.0F, 2.0F, 3.0F, 0.0F, false); //color_tube
+            colour_front.texOffs(9, 7).addBox(-0.5F, -3.5F, -2.0F, 1.0F, 1.0F, 0.0F, 0.0F, false); //color_front
 
             ModelRenderer colour_base = new ModelRenderer(this);
-            colour_base.setPos(0.0F, 24.0F, 0.0F);
-            colour_base.texOffs(-2, 0).addBox(-0.5F, -5.5F, 3.0F, 1.0F, 0.0F, 2.0F, 0.0F, false); //color_top
+            colour_base.setPos(0.0F, 24.0F, 0.0F); // Light at the top
+            colour_base.texOffs(1, 12).addBox(-0.5F, -5.5F, 3.0F, 1.0F, 0.0F, 2.0F, 0.0F, false); //color_top
 
             colour.addChild(colour_front);
             colour.addChild(colour_base);
@@ -101,13 +125,13 @@ public class PortalGunModel extends AnimatedModel {
             accent_1.setPos(-2.0F, -4.0F, 5.0F);
             stripes_base.addChild(accent_1);
             setRotationAngle(accent_1, 0.0F, 1.5708F, 0.0F);
-            accent_1.texOffs(18, 0).addBox(-3.0F, -1.5F, -0.5F, 6.0F, 3.0F, 1.0F, 0.004F, true);
+            accent_1.texOffs(1, 1).addBox(-3.0F, -1.5F, -0.5F, 6.0F, 3.0F, 1.0F, 0.004F, true);
 
             ModelRenderer accent_2 = new ModelRenderer(this);
             accent_2.setPos(2.0F, -4.0F, 5.0F);
             stripes_base.addChild(accent_2);
             setRotationAngle(accent_2, 0.0F, -1.5708F, 0.0F);
-            accent_2.texOffs(18, 0).addBox(-3.0F, -1.5F, -0.5F, 6.0F, 3.0F, 1.0F, 0.004F, false);
+            accent_2.texOffs(1, 1).addBox(-3.0F, -1.5F, -0.5F, 6.0F, 3.0F, 1.0F, 0.004F, false);
 
             stripes.addChild(stripes_base);
         }
