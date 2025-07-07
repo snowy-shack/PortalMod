c518c6f9d248992f2b40e56976b5fdd9f7a0fd39
serialsniper <mome.broili@gmail.com>
2025-07-07 19:13:16 +0200

    Portalgun Crosshair

--- src/main/java/net/portalmod/common/sorted/portalgun/PortalGunCrosshairRenderer.java
+++ src/main/java/net/portalmod/common/sorted/portalgun/PortalGunCrosshairRenderer.java
@@ -2,6 +2,7 @@ package net.portalmod.common.sorted.portalgun;
 
 import com.mojang.blaze3d.matrix.MatrixStack;
 import com.mojang.blaze3d.systems.RenderSystem;
+import net.minecraft.client.MainWindow;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.BufferBuilder;
 import net.minecraft.client.renderer.Tessellator;
@@ -19,206 +20,103 @@ import net.minecraft.util.math.vector.Vector3d;
 import net.minecraft.world.GameType;
 import net.minecraft.world.World;
 import net.portalmod.PortalMod;
+import net.portalmod.client.screens.PortalModOptionsScreen;
 import net.portalmod.common.sorted.portal.PortalEnd;
 import net.portalmod.common.sorted.portal.PortalManager;
 import net.portalmod.core.init.BlockTagInit;
 import net.portalmod.core.util.Colour;
 
+import java.util.UUID;
+
 public class PortalGunCrosshairRenderer {
-//    public static final ResourceLocation CROSSHAIRS = new ResourceLocation(PortalMod.MODID, "textures/crosshairs.png");
-//    public static final ResourceLocation CROSSHAIRS =
-//            new ResourceLocation(PortalMod.MODID, "textures/gui/crosshair/crosshair_normal.png");
-//    public static final ResourceLocation CROSSHAIRS_P1 =
-//            new ResourceLocation(PortalMod.MODID, "textures/gui/crosshair/crosshair_p1.png");
-    
+    private static final String BASE = "textures/gui/crosshair/portalgun_crosshair_";
+
     public static void render(MatrixStack matrixStack) {
-        ItemStack itemStack = Minecraft.getInstance().player.getMainHandItem();
+        Minecraft mc = Minecraft.getInstance();
 
-        int style = 2;
+        if(mc.level == null || mc.player == null || mc.gameMode == null)
+            return;
 
-        if(itemStack.getItem() instanceof PortalGun
-                && Minecraft.getInstance().options.getCameraType().isFirstPerson()
-                && Minecraft.getInstance().gameMode.getPlayerMode() != GameType.SPECTATOR) {
-            int u = 0;
-            int v = 0;
-            
-//            if(!PortalGun.hasPortal(itemStack, PortalEnd.PRIMARY, true))
-//                u = 33;
-//            if(!PortalGun.hasPortal(itemStack, PortalEnd.SECONDARY, true))
-//                v = 33;
-            
-//            BlockRayTraceResult rayTrace = ModUtil.rayTraceBlock(Minecraft.getInstance().player, Minecraft.getInstance().level, 100);
+        ItemStack itemStack = mc.player.getMainHandItem();
+        boolean isClassicCrosshair = PortalModOptionsScreen.CROSSHAIR.get();
+        boolean isFirstPerson = mc.options.getCameraType().isFirstPerson();
+        boolean isSpectator = mc.gameMode.getPlayerMode() == GameType.SPECTATOR;
 
-            PlayerEntity player = Minecraft.getInstance().player;
-            World level = Minecraft.getInstance().level;
+        if(!(itemStack.getItem() instanceof PortalGun) || !isFirstPerson || isSpectator)
+            return;
 
-            Vector3d rayPath = player.getViewVector(0).scale(100);
+        UUID gunUUID = PortalGun.getUUID(itemStack);
+        boolean primaryFilled;
+        boolean secondaryFilled;
+
+        if(isClassicCrosshair) {
+            PlayerEntity player = mc.player;
+            World level = mc.level;
+
+            Vector3d rayPath = player.getViewVector(0).scale(mc.gameRenderer.getRenderDistance());
             Vector3d from = player.getEyePosition(0);
             Vector3d to = from.add(rayPath);
 
             RayTraceContext rayCtx = new RayTraceContext(from, to, RayTraceContext.BlockMode.COLLIDER, RayTraceContext.FluidMode.ANY, null);
             BlockRayTraceResult rayTrace = level.clip(rayCtx);
 
-
-
-
-            boolean b = BlockTagInit.isPortalable(Minecraft.getInstance().level.getBlockState(rayTrace.getBlockPos()).getBlock());
-            
-            // TODO only where placeable
-            if(style == 1)
-                u = b ? 33 : 0;
-
-            if(style == 2)
-//                u = PortalManager.has(PortalGun.getUUID(itemStack), PortalEnd.PRIMARY) ? 33 : 0;
-                u = PortalManager.clientHas(PortalGun.getUUID(itemStack), PortalEnd.PRIMARY) ? 33 : 0;
-
-//            if(style == 2)
-//                u = PortalPairCache.CLIENT.has(PortalGun.getUUID(itemStack), PortalEnd.PRIMARY) ? 33 : 0;
-            
-//            u = PortalGun.hasPortal(itemStack, PortalEnd.PRIMARY, true) ? 33 : 0;
-            v = 0;
-            
-//            Colour colour = Colour.fromHSV((int)System.currentTimeMillis() / 10 % 360, .8f, 1);
-//            Colour oppositeColour = Colour.fromHSV((int)(System.currentTimeMillis() / 10 + 180) % 360, .8f, 1);
-//            Colour colour = new Colour(Color.HSBtoRGB((int)(System.currentTimeMillis() / 10 % 360) / 360f, .8f, 1));
-//            Colour oppositeColour = new Colour(Color.HSBtoRGB((int)((System.currentTimeMillis() / 10 + 180) % 360) / 360f, .8f, 1));
-
-            CompoundNBT nbt = itemStack.getOrCreateTag();
-
-            DyeColor leftColour = PortalGun.getLeftDyeColour(nbt);
-            DyeColor rightColour = PortalGun.getRightDyeColour(nbt);
-
-            ResourceLocation leftTexture = new ResourceLocation(PortalMod.MODID, "textures/gui/crosshair/portalgun_crosshair_" + leftColour.getName() + ".png");
-            ResourceLocation rightTexture = new ResourceLocation(PortalMod.MODID, "textures/gui/crosshair/portalgun_crosshair_" + rightColour.getName() + ".png");
-
-//            if(nbt.contains("portalHue")) {
-//                float hue = (nbt.getInt("portalHue") % 360) / 360.0f;
-//                colour = new Colour(Color.HSBtoRGB(hue, .8f, 1));
-//                oppositeColour = new Colour(Color.HSBtoRGB(hue + .5f, .8f, 1));
-
-//                int color = DyeColor.valueOf(nbt.getString("leftColor").toUpperCase()).getColorValue();
-//                float[] hsv = new float[3];
-//                Color.RGBtoHSB((color >> 16) & 0xFF, (color >> 8) & 0xFF, color & 0xFF, hsv);
-//                leftColour = new Colour(Color.HSBtoRGB(hsv[0], .8f, 1));
-//                rightColour = new Colour(Color.HSBtoRGB(hsv[0] + .5f, .8f, 1));
-//            }
-
-            // blit(stack, x, y, z, u, v, uWidth, uHeight, texwidth, texheight)
-            RenderSystem.disableBlend();
-            Minecraft.getInstance().getTextureManager().bind(leftTexture);
-            blit(matrixStack,
-                    Minecraft.getInstance().getWindow().getGuiScaledWidth() / 2 - 17,
-                    Minecraft.getInstance().getWindow().getGuiScaledHeight() / 2 - 16,
-                    0,
-                    Colour.WHITE, // placeholder, please remove the colour parameter for me snipy :3
-                    u, v, 33, 33, 66, 66);
-            RenderSystem.enableBlend();
-            
-//            u = PortalGun.hasPortal(itemStack, PortalEnd.SECONDARY, true) ? 33 : 0;
-            v = 33;
-
-
-            if(style == 2)
-//                u = PortalManager.has(PortalGun.getUUID(itemStack), PortalEnd.SECONDARY) ? 33 : 0;
-                u = PortalManager.clientHas(PortalGun.getUUID(itemStack), PortalEnd.SECONDARY) ? 33 : 0;
-
-//            if(style == 2)
-//                u = PortalPairCache.CLIENT.has(PortalGun.getUUID(itemStack), PortalEnd.SECONDARY) ? 33 : 0;
-            
-            RenderSystem.disableBlend();
-            Minecraft.getInstance().getTextureManager().bind(rightTexture);
-            blit(matrixStack,
-                    Minecraft.getInstance().getWindow().getGuiScaledWidth() / 2 - 17,
-                    Minecraft.getInstance().getWindow().getGuiScaledHeight() / 2 - 16,
-                    0,
-                    Colour.WHITE, // placeholder, please remove the colour parameter for me snipy :3
-                    u, v, 33, 33, 66, 66);
-            RenderSystem.enableBlend();
-            
-//            RenderSystem.disableBlend();
-//            Minecraft.getInstance().getTextureManager().bind(CROSSHAIRS_P1);
-//            blit(matrixStack,
-//                    Minecraft.getInstance().getWindow().getGuiScaledWidth() / 2 - 17,
-//                    Minecraft.getInstance().getWindow().getGuiScaledHeight() / 2 - 16,
-//                    0,
-//                    new Colour(0xFFFFFF00),
-//                    u, v, 33, 33, 66, 66);
-//            RenderSystem.enableBlend();
-
-            if(style == 1) {
-                v = 0;
-                u = 0;
-
-//                if(PortalPairCache.CLIENT.has(PortalGun.getUUID(itemStack), PortalEnd.PRIMARY)) {
-//                    // blit(stack, x, y, z, u, v, uWidth, uHeight, texwidth, texheight)
-//                    RenderSystem.disableBlend();
-//                    Minecraft.getInstance().getTextureManager().bind(CROSSHAIRS_P1);
-//                    blit(matrixStack,
-//                            Minecraft.getInstance().getWindow().getGuiScaledWidth() / 2 - 17,
-//                            Minecraft.getInstance().getWindow().getGuiScaledHeight() / 2 - 16,
-//                            0,
-//                            colour,
-//                            u, v, 33, 33, 66, 33);
-//                    RenderSystem.enableBlend();
-//                }
-
-//            u = PortalGun.hasPortal(itemStack, PortalEnd.SECONDARY, true) ? 33 : 0;
-                u = 33;
-
-//                if(PortalPairCache.CLIENT.has(PortalGun.getUUID(itemStack), PortalEnd.SECONDARY)) {
-//                    RenderSystem.disableBlend();
-//                    Minecraft.getInstance().getTextureManager().bind(CROSSHAIRS_P1);
-//                    blit(matrixStack,
-//                            Minecraft.getInstance().getWindow().getGuiScaledWidth() / 2 - 17,
-//                            Minecraft.getInstance().getWindow().getGuiScaledHeight() / 2 - 16,
-//                            0,
-//                            oppositeColour,
-//                            u, v, 33, 33, 66, 33);
-//                    RenderSystem.enableBlend();
-//                }
-            }
+            boolean isPortalable = BlockTagInit.isPortalable(mc.level.getBlockState(rayTrace.getBlockPos()).getBlock());
+            primaryFilled = isPortalable;
+            secondaryFilled = isPortalable;
+            // todo actually figure out whether the portal is placeable there
+        } else {
+            primaryFilled = PortalManager.clientHas(gunUUID, PortalEnd.PRIMARY);
+            secondaryFilled = PortalManager.clientHas(gunUUID, PortalEnd.SECONDARY);
         }
+
+        CompoundNBT nbt = itemStack.getOrCreateTag();
+        DyeColor primaryColor = PortalGun.getLeftDyeColour(nbt);
+        DyeColor secondaryColor = PortalGun.getRightDyeColour(nbt);
+        renderCrosshairPart(matrixStack, PortalEnd.PRIMARY, primaryColor, primaryFilled);
+        renderCrosshairPart(matrixStack, PortalEnd.SECONDARY, secondaryColor, secondaryFilled);
     }
-    
-    public static void blit(MatrixStack matrixStack, int x, int y, int z, Colour colour, float u0, float v0, int uw, int uh, int width, int height) {
+
+    private static void renderCrosshairPart(MatrixStack matrixStack, PortalEnd end, DyeColor color, boolean filled) {
+        ResourceLocation texture = new ResourceLocation(PortalMod.MODID, BASE + color.getName() + ".png");
+        MainWindow window = Minecraft.getInstance().getWindow();
+        int x = -17;
+        int y = -16;
+        int size = 33;
+
+        RenderSystem.disableBlend();
+        Minecraft.getInstance().getTextureManager().bind(texture);
+        blit(matrixStack, window.getGuiScaledWidth() / 2 + x, window.getGuiScaledHeight() / 2 + y, 0,
+                filled ? size : 0, end == PortalEnd.PRIMARY ? 0 : size, size, size, size * 2, size * 2);
+        RenderSystem.enableBlend();
+    }
+
+    private static void blit(MatrixStack matrixStack, int x, int y, int z, float u0, float v0, int uw, int uh, int width, int height) {
         innerBlit(matrixStack,
                 x, x + uw,
                 y, y + uh,
                 z,
-                colour,
                 uw, uh, u0, v0,
                 width, height);
     }
     
-    private static void innerBlit(MatrixStack matrixStack, int x0, int x1, int y0, int y1, int z, Colour colour, int uw, int uh, float u0, float v0, int width, int height) {
+    private static void innerBlit(MatrixStack matrixStack, int x0, int x1, int y0, int y1, int z, int uw, int uh, float u0, float v0, int width, int height) {
         innerBlit(matrixStack.last().pose(),
                 x0, x1, y0, y1, z,
-                colour,
                 (u0 + 0) / (float)width,
                 (u0 + uw) / (float)width,
                 (v0 + 0) / (float)height,
                 (v0 + uh) / (float)height);
     }
     
-    private static void innerBlit(Matrix4f matrix, int x0, int x1, int y0, int y1, int z, Colour colour, float u0, float u1, float v0, float v1) {
+    private static void innerBlit(Matrix4f matrix, int x0, int x1, int y0, int y1, int z, float u0, float u1, float v0, float v1) {
         BufferBuilder bufferbuilder = Tessellator.getInstance().getBuilder();
         bufferbuilder.begin(7, DefaultVertexFormats.POSITION_COLOR_TEX);
-        bufferbuilder.vertex(matrix, (float)x0, (float)y1, (float)z).color(colour.getIntR(), colour.getIntG(), colour.getIntB(), colour.getIntA()).uv(u0, v1).endVertex();
-        bufferbuilder.vertex(matrix, (float)x1, (float)y1, (float)z).color(colour.getIntR(), colour.getIntG(), colour.getIntB(), colour.getIntA()).uv(u1, v1).endVertex();
-        bufferbuilder.vertex(matrix, (float)x1, (float)y0, (float)z).color(colour.getIntR(), colour.getIntG(), colour.getIntB(), colour.getIntA()).uv(u1, v0).endVertex();
-        bufferbuilder.vertex(matrix, (float)x0, (float)y0, (float)z).color(colour.getIntR(), colour.getIntG(), colour.getIntB(), colour.getIntA()).uv(u0, v0).endVertex();
+        bufferbuilder.vertex(matrix, (float)x0, (float)y1, (float)z).color(1f, 1f, 1f, 1f).uv(u0, v1).endVertex();
+        bufferbuilder.vertex(matrix, (float)x1, (float)y1, (float)z).color(1f, 1f, 1f, 1f).uv(u1, v1).endVertex();
+        bufferbuilder.vertex(matrix, (float)x1, (float)y0, (float)z).color(1f, 1f, 1f, 1f).uv(u1, v0).endVertex();
+        bufferbuilder.vertex(matrix, (float)x0, (float)y0, (float)z).color(1f, 1f, 1f, 1f).uv(u0, v0).endVertex();
         bufferbuilder.end();
         RenderSystem.enableAlphaTest();
         WorldVertexBufferUploader.end(bufferbuilder);
-        
-//        BufferBuilder bufferbuilder = Tessellator.getInstance().getBuilder();
-//        bufferbuilder.begin(7, DefaultVertexFormats.POSITION_TEX);
-//        bufferbuilder.vertex(matrix, (float)x0, (float)y1, (float)z).uv(u0, v1).endVertex();
-//        bufferbuilder.vertex(matrix, (float)x1, (float)y1, (float)z).uv(u1, v1).endVertex();
-//        bufferbuilder.vertex(matrix, (float)x1, (float)y0, (float)z).uv(u1, v0).endVertex();
-//        bufferbuilder.vertex(matrix, (float)x0, (float)y0, (float)z).uv(u0, v0).endVertex();
-//        bufferbuilder.end();
-//        RenderSystem.enableAlphaTest();
-//        WorldVertexBufferUploader.end(bufferbuilder);
     }
 }
