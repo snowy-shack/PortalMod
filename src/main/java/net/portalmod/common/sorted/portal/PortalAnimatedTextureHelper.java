10c1de2d3d93058d288ea4efb201ef7d7a41b1b8
serialsniper <serialsniper.official@gmail.com>
2025-12-14 19:01:14 +0100

    Portal opening animation

--- src/main/java/net/portalmod/common/sorted/portal/PortalAnimatedTextureHelper.java
+++ src/main/java/net/portalmod/common/sorted/portal/PortalAnimatedTextureHelper.java
@@ -7,19 +7,19 @@ import net.minecraft.util.ResourceLocation;
 
 import java.awt.*;
 import java.util.HashMap;
+import java.util.Optional;
 
 public class PortalAnimatedTextureHelper {
-    private static final HashMap<ResourceLocation, Dimension> TEXTURE_SIZES = new HashMap<>();
+    private static final HashMap<ResourceLocation, Optional<Dimension>> TEXTURE_SIZES = new HashMap<>();
 
-    public static Dimension getTextureSize(ResourceLocation location) {
+    public static Optional<Dimension> getTextureSize(ResourceLocation location) {
         IResourceManager rm = Minecraft.getInstance().getResourceManager();
 
         if(!TEXTURE_SIZES.containsKey(location)) {
             try(SimpleTexture.TextureData data = SimpleTexture.TextureData.load(rm, location)) {
-                TEXTURE_SIZES.put(location, new Dimension(data.getImage().getWidth(), data.getImage().getHeight()));
+                TEXTURE_SIZES.put(location, Optional.of(new Dimension(data.getImage().getWidth(), data.getImage().getHeight())));
             } catch(Exception e) {
-                e.printStackTrace();
-                return new Dimension(0, 0);
+                TEXTURE_SIZES.put(location, Optional.empty());
             }
         }
 
