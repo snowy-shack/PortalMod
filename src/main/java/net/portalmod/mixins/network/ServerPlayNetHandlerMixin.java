468aae0d6983e6b2b3b830fd1890db33acc3e765
serialsniper <serialsniper.official@gmail.com>
2025-12-25 01:30:44 +0100

    Roughly fixed floor portal stuckness

--- src/main/java/net/portalmod/mixins/network/ServerPlayNetHandlerMixin.java
+++ src/main/java/net/portalmod/mixins/network/ServerPlayNetHandlerMixin.java
@@ -1,11 +1,12 @@
 package net.portalmod.mixins.network;
 
+import net.minecraft.entity.player.ServerPlayerEntity;
 import net.minecraft.network.play.ServerPlayNetHandler;
 import net.portalmod.common.sorted.portal.IClientTeleportable;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.ModifyVariable;
+import org.spongepowered.asm.mixin.injection.Redirect;
 
 @Mixin(ServerPlayNetHandler.class)
 public class ServerPlayNetHandlerMixin {
@@ -22,5 +23,18 @@ public class ServerPlayNetHandlerMixin {
             return false;
         }
         return value;
+    }
+
+    @Redirect(
+            remap = false,
+            method = "handleMovePlayer",
+            at = @At(
+                    value = "INVOKE",
+                    target = "Lnet/minecraft/entity/player/ServerPlayerEntity;isSleeping()Z",
+                    ordinal = 2
+            )
+    )
+    private boolean pmAllowPortalTeleportation(ServerPlayerEntity player) {
+        return true; // todo actual logic to check tp validity here
     }
 }
