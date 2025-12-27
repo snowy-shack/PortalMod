2c533ada8d3c9347a2602b0b10b841d927bf1312
LarsMans <l.s.vanemmerik@gmail.com>
2025-12-27 17:44:35 +0100

    ok i didnt completely fix it but at least it cannot go on forever anymore

--- src/main/java/net/portalmod/common/sorted/portalgun/PortalGunGrabSoundClient.java
+++ src/main/java/net/portalmod/common/sorted/portalgun/PortalGunGrabSoundClient.java
@@ -2,6 +2,7 @@ package net.portalmod.common.sorted.portalgun;
 
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.audio.LocatableSound;
+import net.minecraft.client.audio.SoundHandler;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.util.SoundCategory;
 import net.portalmod.core.init.SoundInit;
@@ -14,11 +15,17 @@ public class PortalGunGrabSoundClient {
 
     // TODO this is not a real packet
     public static void handlePacket(PlayerEntity player, boolean start) {
-        if (start) {
+        SoundHandler soundManager = Minecraft.getInstance().getSoundManager();
+        boolean hasSound = SOUNDS.containsKey(player);
+
+        if (start && !hasSound) {
             SOUNDS.put(player, new EntityLoopableSound(player, SoundInit.PORTALGUN_HOLD.get(), SoundCategory.PLAYERS));
-            Minecraft.getInstance().getSoundManager().play(SOUNDS.get(player));
-        } else {
-            Minecraft.getInstance().getSoundManager().stop(SOUNDS.get(player));
+            soundManager.play(SOUNDS.get(player));
+        }
+
+        if (!start && hasSound) {
+            soundManager.stop(SOUNDS.get(player));
+            SOUNDS.remove(player);
         }
     }
 }
