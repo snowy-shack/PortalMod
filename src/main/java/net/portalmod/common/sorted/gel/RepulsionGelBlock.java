f6a5f4a0aa02218483a12b3fe6aadd926e65e76f
serialsniper <serialsniper.official@gmail.com>
2026-03-25 01:22:16 +0100

    Fixed repulsion gel server crash (thank you phant)

--- src/main/java/net/portalmod/common/sorted/gel/RepulsionGelBlock.java
+++ src/main/java/net/portalmod/common/sorted/gel/RepulsionGelBlock.java
@@ -122,7 +122,7 @@ public class RepulsionGelBlock extends AbstractGelBlock {
         boolean bounceFromSpeed = entity.getDeltaMovement().length() > 0.2 && entity.isOnGround();
         boolean bounceFromJump = gelAffected.getWasOnGround() && entity.getDeltaMovement().y > 0.2;
 
-        double heightInBlock = entity.getPosition(1).y % 1;
+        double heightInBlock = entity.position().y;
         if (heightInBlock > 0.5) return; // Fix (#51)
 
         // Vertical bounce
